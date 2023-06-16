5016a0c255bf7c089d6896bd2628b1b4f5692cc5
RENATO DOUGLAS <83033433+renatdoug@users.noreply.github.com>
2023-06-16 11:41:50 -0300

    Adicionando Icone

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QVBoxLayout, QWidget
-
+from PySide6.QtWidgets import QApplication, QLabel
 from main_window import MainWindow
+from PySide6.QtGui import QIcon
+from variaables import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
+
+    #Definindo o icone
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
+
+    label1 = QLabel('Renato Douglas')
+    label1.setStyleSheet('font-size: 50px;color: red')
+
+    #Resolvendo o problema da profundidade de nível     window.v_layout.addWidget(label1)
+    window.addWidgetToLayout(label1)
+
+    window.adjustFixedSize()
+
     window.show()
     app.exec_()
--- main_window.py
+++ main_window.py
@@ -1,16 +1,30 @@
 import sys
-from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QVBoxLayout, QWidget)
+
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget
 
 
 class MainWindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs ) -> None:
         super().__init__(parent, *args, **kwargs)
 
+        # Configurado o layout básico
         self.cw = QWidget()
         self.v_layout = QVBoxLayout()
-        self.cw.setLayout(self.v_layout)
-
-        self.label1 = QLabel('Renato Douglas')
-        self.v_layout.addWidget(self.label1)
-
+        self.cw.setLayout(self.v_layout) 
         self.setCentralWidget(self.cw)
+
+        #Titulo da janela
+        self.setWindowTitle('Calcuradora')
+
+          
+    def adjustFixedSize(self):
+        #Última coisa a ser feita
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
+    
+    #Resolvendo a profundidade dos níveis
+
+    def addWidgetToLayout(self, widget:QWidget):
+        self.v_layout.addWidget(widget)
+       
